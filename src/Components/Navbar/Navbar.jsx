84cbae215780898ed191c3de45304957f12febd2
Leonardo Bezerra <109699627+leobez@users.noreply.github.com>
2023-09-04 10:44:54 -0300

    Geral nas pastas

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,42 +1,12 @@
 import React from 'react'
-import { NavLink } from "react-router-dom"
 import styles  from "./Navbar.module.css"
 
 const Navbar = () => {
-
-
+	
     return (
-		<nav className={styles.navbar}>
-			<NavLink className={styles.brand} to="/">
-				TCCCC <span>Campo</span>
-			</NavLink>
-			<ul className={styles.links_list}>
-				<li>
-					<NavLink
-					to="/"
-					className={({ isActive }) => (isActive ? styles.active : "")}
-					>
-					Home
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-					to="/login"
-					className={({ isActive }) => (isActive ? styles.active : "")}
-					>
-					Login
-					</NavLink>
-				</li>
-				<li>
-					<NavLink
-					to="/cadastro"
-					className={({ isActive }) => (isActive ? styles.active : "")}
-					>
-					Cadastro
-					</NavLink>
-				</li>
-			</ul>
-	  </nav>
+		<nav>
+
+	  	</nav>
 	)
 }
 
