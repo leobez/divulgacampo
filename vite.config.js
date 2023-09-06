62e487a1b3094a794b664046f9e9e6c35ad0cb44
Leonardo Bezerra <leossbezerra@gmail.com>
2023-09-06 14:24:42 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/"
-  //base: "/projeto-lab-eng-soft" -> USE WHEN DEPLOY FOR gh-pages
+  //base: "/projeto-lab-eng-soft" -> USE WHEN DEPLOY FOR gh-pages teste
 })
