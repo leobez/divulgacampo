7fd414799dfaf4a9ac2aeb53d946c30974184bbe
Leonardo Bezerra <leossbezerra@gmail.com>
2023-09-13 23:27:13 -0300

    little more of css

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import styles from "./Home.module.css"
 import AuthContext from '../../Context/AuthContext'
+import { Link } from 'react-router-dom'
 
 const Home = ({isEmailVerified}) => {
 
@@ -8,8 +9,19 @@ const Home = ({isEmailVerified}) => {
 
 	return (
 		<div className={styles.home}>
-			<p> Olá, você esta na tela de home! </p> 
-			{!auth.currentUser && <p>Cadastre-se!</p>}		
+			<div className={styles.welcome}>
+				<p> Olá, você esta na tela de home! </p> 
+
+				{!auth.currentUser && 
+					<div className={styles.welcomeSubdivision}>
+						{!auth.currentUser && <Link to="/register" className={styles.homeLink}>Cadastre-se</Link>}
+						<p> ou faça </p>
+						{!auth.currentUser && <Link to="/login" className={styles.homeLink}>Login</Link>}
+					</div>
+				}
+			</div>
+			<hr />
+	
 		</div>
 	)
 }
