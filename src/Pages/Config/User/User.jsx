8b05401a2a71f9d15372459dd57a02193c7f3489
Leonardo Bezerra <leossbezerra@gmail.com>
2023-09-30 10:55:51 -0300

    a little css for config user

--- src/Pages/Config/User/User.jsx
+++ src/Pages/Config/User/User.jsx
@@ -1,19 +1,30 @@
 import React, { useContext } from 'react'
 import AuthContext from '../../../Context/AuthContext'
+import styles from './User.module.css'
 
 const User = () => {
 	
 	const auth = useContext(AuthContext)
 
 	return (
-		<div>
+		<div className={styles.configuser}> 
 			<p>Configuração de usuário</p>
 			<hr />
 			{ auth.currentUser &&
 				<>
-					<p>Trocar de senha </p>
-					<p>Trocar de email: <span>{auth.currentUser.email}</span></p>
-					<p>Trocar displayName:  <span>{auth.currentUser.displayName}</span></p>
+					<div>
+						<button>Trocar de senha </button>
+					</div>
+
+					<div>
+						<button>Trocar de email </button>
+						<span>{auth.currentUser.email}</span>
+					</div>
+
+					<div>
+						<button>Trocar displayName </button>
+						<span>{auth.currentUser.displayName}</span>
+					</div>
 				</>
 			}
 			
