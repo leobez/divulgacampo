04393b145bdd6546e06ba2c1d7113e550337bf48
Leonardo Bezerra <109699627+leobez@users.noreply.github.com>
2023-10-09 09:45:37 -0300

    created forgotpassword form

--- src/Pages/ForgotPassword/ForgotPassword.jsx
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -1,10 +1,43 @@
 import React from 'react'
 import styles from "./ForgotPassword.module.css"
+import { useState } from 'react'
 
 const ForgotPassword = () => {
+
+	const [email, setEmail] = useState(undefined)
+	const [error, setError] = useState("")
+
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		console.log(email)
+	}
+
 	return (
 		<div className={styles.forgotpassword}>
-			ForgotPassword
+			 <div>
+				<form onSubmit={handleSubmit} className={styles.form}>
+					<div className='formtitle'>
+						<h1>Mude sua senha</h1>
+					</div>
+
+					<div>
+						<label htmlFor="email">E-mail: </label>
+						<input 
+						placeholder='Digite seu email'
+						type="email" 
+						name="email" 
+						onChange={(e) => setEmail(e.target.value)}/>
+					</div>
+
+					{/* {!loading ? (<input type="submit" value="Enviar"/>):(
+					<input type="submit" value="Carregando..." className='loadingButton' disabled/>)} */}
+					<input type="submit" value="Enviar"/>
+
+					<div className="error">
+						{error && <p>{error}</p>}
+					</div>
+				</form>
+			</div>
 		</div>
 	)
 }
