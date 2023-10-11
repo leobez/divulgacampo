4991f9ddb86f2ed6ae9f85c23e4198eefbd77ddc
Leonardo Bezerra <leossbezerra@gmail.com>
2023-10-10 22:28:19 -0300

    removed email.js

--- src/Hooks/useSendEmail.jsx
+++ src/Hooks/useSendEmail.jsx
@@ -1,25 +1,13 @@
 import { useState } from "react"
-import { emailjs } from "../emailjs/config"
 
 export const useSendEmail = () => {
 
 	const [error, setError] = useState("")
 	const [loading, setLoading] = useState(false)
 
-	const sendEmail = async({serviceId, templateId, templateParams}) => {
-		try {
-			const res = await emailjs.send(serviceId, templateId, templateParams)
-			console.log(res)
-		} catch (error) {
-			console.log(error)
-			setError("Algo deu errado.")
-		}	
-	}
-
-
 	return {
 		error,
 		loading,
-		sendEmail
+		
 	}
 } 
--- src/emailjs/config.js
+++
@@ -1,7 +0,0 @@
-import emailjs from '@emailjs/nodejs'
-
-emailjs.init({
-	publicKey: 'ATJr_b9J1-LMfRKHd'
-})
-
-export {emailjs}
