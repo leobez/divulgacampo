1842c9736bd4c744b28552e96f5c3faf68b13aed
Leonardo Bezerra <leossbezerra@gmail.com>
2023-10-12 11:15:07 -0300

    user config css

--- src/Pages/Config/User/User.jsx
+++ src/Pages/Config/User/User.jsx
@@ -5,34 +5,35 @@ import { Link } from 'react-router-dom'
 
 const User = () => {
 
-
 	const auth = useContext(AuthContext)
 
 	return (
 		<div className={styles.configuser}> 
+
 			{!auth.currentUser && <p>Cadastre para ter acesso a essas configurações!</p>}
 			{ auth.currentUser &&
 				<>
 					<h1>Suas informações: </h1>
 					<hr />
 					<p className={styles.info}>Seu id é: <span>{auth.currentUser.uid}</span></p>
-					<p className={styles.info}>Seu email é: <span>{auth.currentUser.email}</span></p>
 					<p className={styles.info}>Seu nome é: <span>{auth.currentUser.displayName}</span></p>
+					<p className={styles.info}>Seu email é: <span>{auth.currentUser.email}</span></p>
 					<hr />
+					
 					<div>
-						<Link className='nonNavLink' to="/changename">Trocar nome </Link>
+						<Link to="/changename">Trocar nome </Link>
 					</div>
 
 					<div>
-						<Link className='nonNavLink' to="/changeemail"> Trocar email </Link>
+						<Link to="/changeemail"> Trocar email </Link>
 					</div>
 
 					<div>
-						<Link className='nonNavLink' to="/changepassword">Trocar senha </Link>
+						<Link to="/changepassword"> Trocar senha </Link>
 					</div>
 
 					<div>
-						<Link className='nonNavLink' to="/deleteaccount"> Excluir conta </Link>
+						<Link to="/deleteaccount"> Excluir conta </Link>
 					</div>
 				</>
 			}
