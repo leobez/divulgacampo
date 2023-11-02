164aae5c92f164cbdf7b78ab3e61db76b530c120
Leonardo Bezerra <leossbezerra@gmail.com>
2023-11-02 10:23:59 -0300

    Update useUpdateDocument.jsx

--- src/Hooks/useUpdateDocument.jsx
+++ src/Hooks/useUpdateDocument.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import {db} from "../firebase/config"
-import { Timestamp, addDoc, collection, doc, updateDoc } from "firebase/firestore"
+import { doc, updateDoc } from "firebase/firestore"
 import { useNavigate } from "react-router-dom"
 
 export const useUpdateDocument = (collectionName) => {
