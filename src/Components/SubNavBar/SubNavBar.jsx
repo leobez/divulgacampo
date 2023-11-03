74922b9febfbe4ed9d57e69af3ec09482c26b1a0
Leonardo Bezerra <leossbezerra@gmail.com>
2023-11-03 10:55:36 -0300

    changes on menu/contact routing

--- src/Components/SubNavBar/SubNavBar.jsx
+++ src/Components/SubNavBar/SubNavBar.jsx
@@ -1,18 +1,24 @@
 import React from 'react'
 import styles from './SubNavBar.module.css'
 import { NavLink } from 'react-router-dom'
+import { useContext } from 'react'
+import AuthContext from '../../Context/AuthContext'
 
 const SubNavBar = () => {
 
+	const auth = useContext(AuthContext)
+
 	return (
 		<div className={styles.subnavbar}>
+			
 			<NavLink to='/config'>
 				Menu
 			</NavLink>
 
-			<NavLink to='/contact'>
-				Contato
-			</NavLink>
+			<NavLink to='/contact'> 
+				Contato 
+			</NavLink> 
+			
 		</div>
 	)
 }
