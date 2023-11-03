4981a0072b7cb60b6b36409080f5586869922632
Leonardo Bezerra <leossbezerra@gmail.com>
2023-11-03 11:30:40 -0300

    created contact screen

--- src/Hooks/useSendContactEmail.jsx
+++ src/Hooks/useSendContactEmail.jsx
@@ -1,30 +1,41 @@
 import { useState } from "react"
+import emailjs from '@emailjs/browser';
+import emailjsconfig from '../emailjs/config'
 
 export const useSendContactEmail = () => {
 
 
 	const [apiError, setApiError] = useState("")
+	const [apiSuccess, setApiSuccess] = useState("")
 	const [loading, setLoading] = useState(false)
 
-	const sendEmail = async() => {
+	const sendEmail = async(data) => {
 		setApiError("")
+		setApiSuccess("")
 
 		try {
 			setLoading(true)
-			console.log("chegou")
-			setLoading(false)
+			await emailjs.send(
+				emailjsconfig.Service_ID, 
+				emailjsconfig.Template_ID,
+				data, 
+				emailjsconfig.Public_key
+			)
 
+			setApiSuccess("Email enviado com sucesso.")
+			setLoading(false)
 		} catch (error) {
 			setLoading(false)
 			setApiError("Algo deu errado")
 			console.log(error)
-		}
+		} 
 
 	}
 
 	return {
 		loading, 
 		apiError,
+		apiSuccess,
 		sendEmail
 	}
 }
--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,15 +1,17 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import styles from './Contact.module.css'
 import { useSendContactEmail } from '../../Hooks/useSendContactEmail'
+import AuthContext from '../../Context/AuthContext'
 
 const Contact = () => {
 
 	const [name, setName] = useState("")
-	const [email, setEmail] = useState("")
 	const [message, setMessage] = useState("")
 	const [error, setError] = useState("")
 
-	const {loading, apiError, sendEmail} = useSendContactEmail()
+	const {loading, apiError, apiSuccess, sendEmail} = useSendContactEmail()
+
+	const auth = useContext(AuthContext)
 
 	const handleSubmit = async(e) => {
 		e.preventDefault()
@@ -18,9 +20,6 @@ const Contact = () => {
 			name.trim() === "" || 
 			name.trim() === undefined || 
 			name.trim() === null ||
-			email.trim() === "" || 
-			email.trim() === undefined ||
-			email.trim() === null ||
 			message === "" || 
 			message === undefined ||
 			message === null
@@ -31,7 +30,13 @@ const Contact = () => {
 
 		setError("")
 
-		await sendEmail()
+		const data = {
+			from_name: name,
+			from_email: auth.currentUser.email,
+			message: message,
+		}
+
+		await sendEmail(data)
 
 	}
 
@@ -55,15 +60,6 @@ const Contact = () => {
 					</div>
 
 					<div>
-						<label htmlFor="email">E-mail: </label>
-						<input 
-						placeholder='Digite seu email'
-						type="email" 
-						name="email" 
-						onChange={(e) => setEmail(e.target.value)}/>
-					</div>
-
-					<div>
 						<label htmlFor="message">Mensagem: </label>
 						<textarea 
 							type='text' 
@@ -82,6 +78,10 @@ const Contact = () => {
 						{error && <p><span>{error}</span></p>}
 						{apiError && <p><span>{apiError}</span></p>}
 					</div>
+					<div className="success">
+						{apiSuccess && <p><span>{apiSuccess}</span></p>}
+					</div>
+
 				</form>
 			</div>
 		</div>
